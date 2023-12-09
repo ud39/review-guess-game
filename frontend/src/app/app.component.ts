994ed4b75d707a78fdff68005d21e59204997056
ud39 <ud-gaming@gmx.de>
2023-12-09 01:07:10 +0100

    Change: Remove placeholder content inside .html and add component to .ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,23 @@
+import { User } from './types/MyTypes';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ReviewPerspectiveComponent } from './review-perspective/review-perspective.component';
+import { GuessPerspectiveComponent } from './guess-perspective/guess-perspective.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    GuessPerspectiveComponent,
+    ReviewPerspectiveComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'GuessTheGame';
+  user: User | undefined;
 }
