bd65537a36d2e39d39fa2db73ff6a8b7f7502962
ud39 <ud-gaming@gmx.de>
2023-12-10 19:55:49 +0100

    Add: style for display, change of markup to include h2 of role and example reviews

--- frontend/src/app/guess-perspective/guess-perspective.component.ts
+++ frontend/src/app/guess-perspective/guess-perspective.component.ts
@@ -1,15 +1,44 @@
 import { Component, Input } from '@angular/core';
 import { ReviewCardComponent } from '../review-card/review-card.component';
 import { Review } from '../types/MyTypes';
+import { ScoreComponent } from '../score/score.component';
 
 @Component({
   selector: 'app-guess-perspective',
   standalone: true,
-  imports: [ReviewCardComponent],
+  imports: [ReviewCardComponent, ScoreComponent],
   templateUrl: './guess-perspective.component.html',
   styleUrl: './guess-perspective.component.scss',
 })
 export class GuessPerspectiveComponent {
-  @Input() reviews: Review[] = [];
+  @Input() reviews: Review[] = [
+    {
+      id: 1,
+      title: 'Final Fantasy XV',
+      text: 'Good game',
+      recommend: true,
+      played_hours: 1337,
+      helpful: 20,
+      funny: 69,
+    },
+    {
+      id: 2,
+      title: 'Witcher 3',
+      text: 'Okay game',
+      recommend: true,
+      played_hours: 42,
+      helpful: 20,
+      funny: 69,
+    },
+    {
+      id: 3,
+      title: 'Breath of the Wild',
+      text: 'Amazing game',
+      recommend: true,
+      played_hours: 42,
+      helpful: 20,
+      funny: 69,
+    },
+  ];
   isLoading: boolean = true;
 }
