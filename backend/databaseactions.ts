1e09e67bf6b73ce63daa3fbf93ac3cf13093886a
ud39 <ud-gaming@gmx.de>
2023-12-17 21:37:37 +0100

    Add: Change migration to databaseaction. Insert Game and insert reviews.
        Wrapped calls to python server and aswell as database actions into functions.
        Add two insert function:
        Add game with reviews.
        Add to an existing game more reviews.

---
+++ backend/databaseactions.ts
@@ -0,0 +1,153 @@
+import { PrismaClient } from "@prisma/client";
+import { Prisma } from "@prisma/client";
+import {
+  AppIdRequest,
+  AppInfoResponse,
+  GameNameRequest,
+  ReviewsResponse,
+} from "./steam_scraper";
+import path from "path";
+import { GrpcPromiseFactory, SteamServicePythonGrpc } from "./scraper";
+
+const protoLoader = require("@grpc/proto-loader");
+const grpc = require("@grpc/grpc-js");
+
+const packageDefinitionRec = protoLoader.loadSync(
+  path.join(__dirname, "../../reviews/proto/steam_scraper.proto"),
+);
+const steamScraperProto = grpc.loadPackageDefinition(packageDefinitionRec);
+const steamScraperStub: SteamServicePythonGrpc =
+  new steamScraperProto.scraper_steamreviews.SteamService(
+    "0.0.0.0:50051",
+    grpc.credentials.createInsecure(),
+  );
+
+const grpcPromiseFactory = new GrpcPromiseFactory(steamScraperStub);
+const getAppId = grpcPromiseFactory.createPromisifiedMethod("GetAppId");
+const getReviews = grpcPromiseFactory.createPromisifiedMethod("GetReviews");
+
+const prisma = new PrismaClient();
+
+async function insertGameAndReviews(
+  appInfoResp: AppInfoResponse,
+  reviewsResp: ReviewsResponse,
+): Promise<void> {
+  try {
+    const reviews = reviewsResp.review.map((review) => ({
+      steam_id: Number(review.steamId),
+      review: review.review,
+      voted_up: review.votedUp,
+      votes_up: 0,
+      votes_funny: 0,
+      playtime_forever: review.playtimeForever,
+    }));
+
+    console.log(`Inserting reviews for ${appInfoResp.title}`);
+    console.log(reviews);
+
+    await prisma.game.create({
+      data: {
+        app_id: Number(appInfoResp.appId),
+        title: appInfoResp.title,
+        reviews: {
+          create: reviews,
+        },
+      },
+    });
+  } catch (error) {
+    console.error(`Error in insertReviews: ${error}`);
+    throw error;
+  }
+}
+
+// Raw SQL because currently prisma doesn't support native insert on conflict
+async function addReviews(
+  appInfoResp: AppInfoResponse,
+  reviewsResp: ReviewsResponse,
+): Promise<void> {
+  const values = reviewsResp.review.map((review) => ({
+    steam_id: Number(review.steamId),
+    review: review.review,
+    voted_up: review.votedUp,
+    votes_up: 0,
+    votes_funny: 0,
+    playtime_forever: review.playtimeForever,
+  }));
+
+  const result = await prisma.$executeRaw`INSERT INTO "Review" 
+    (steam_id, app_id, review, voted_up, votes_up, votes_funny, playtime_forever) 
+    VALUES ${Prisma.join(
+      values.map(
+        (exp) =>
+          Prisma.sql`(${exp.steam_id}, 
+           ${Number(appInfoResp.appId)}, 
+           ${exp.review}, ${exp.voted_up}, 
+           ${exp.votes_up}, ${exp.votes_funny}, 
+           ${exp.playtime_forever})`,
+      ),
+    )}
+    ON CONFLICT ("steam_id", "app_id") DO NOTHING`;
+  console.log(result);
+}
+
+export async function insertGameWithReviews(
+  gameNameRequest: GameNameRequest,
+  appIdRequest: AppIdRequest,
+) {
+  const appInfo: Promise<AppInfoResponse> = getAppId(gameNameRequest);
+  const reviews: Promise<ReviewsResponse> = getReviews(appIdRequest);
+
+  try {
+    const [appInfoResult, reviewsResult] = await Promise.allSettled([
+      appInfo,
+      reviews,
+    ]);
+
+    if (
+      appInfoResult.status === "rejected" ||
+      reviewsResult.status === "rejected"
+    ) {
+      throw new Error(
+        `At least one promise was rejected. AppInfo: ${appInfoResult.status} Reviews: ${reviewsResult.status}`,
+      );
+    }
+
+    await insertGameAndReviews(appInfoResult.value, reviewsResult.value);
+    await prisma.$disconnect();
+  } catch (error) {
+    console.error("Error in Promise.allSettled:", error);
+
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+}
+
+export async function addNewReviews(
+  gameNameRequest: GameNameRequest,
+  appIdRequest: AppIdRequest,
+) {
+  const appInfo: Promise<AppInfoResponse> = getAppId(gameNameRequest);
+  const reviews: Promise<ReviewsResponse> = getReviews(appIdRequest);
+
+  try {
+    const [appInfoResult, reviewsResult] = await Promise.allSettled([
+      appInfo,
+      reviews,
+    ]);
+
+    if (
+      appInfoResult.status === "rejected" ||
+      reviewsResult.status === "rejected"
+    ) {
+      throw new Error(
+        `At least one promise was rejected. AppInfo: ${appInfoResult.status} Reviews: ${reviewsResult.status}`,
+      );
+    }
+    await addReviews(appInfoResult.value, reviewsResult.value);
+    await prisma.$disconnect();
+  } catch (error) {
+    console.error("Error in Promise.allSettled:", error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+}
--- backend/server.ts
+++ backend/server.ts
@@ -1,66 +1,9 @@
 import express, { Express, Response } from "express";
-import path from "path";
-import {
-  AppIdRequest,
-  AppInfoResponse,
-  GameNameRequest,
-  NReviewsRequest,
-} from "./steam_scraper";
-import { GrpcPromiseFactory, SteamServicePythonGrpc } from "./scraper";
-
-const protoLoader = require("@grpc/proto-loader");
-const grpc = require("@grpc/grpc-js");
+import { insertGameWithReviews } from "./databaseactions";
+import { AppIdRequest, GameNameRequest } from "./steam_scraper";
 
 const app: Express = express();
 const PORT = process.env["PORT"] || 3000;
-const packageDefinitionRec = protoLoader.loadSync(
-  path.join(__dirname, "../../reviews/proto/steam_scraper.proto"),
-);
-const steamScraperProto = grpc.loadPackageDefinition(packageDefinitionRec);
-
-const steamScraperStub: SteamServicePythonGrpc =
-  new steamScraperProto.scraper_steamreviews.SteamService(
-    "0.0.0.0:50051",
-    grpc.credentials.createInsecure(),
-  );
-
-const promisifiedGetAppId = (
-  request: GameNameRequest,
-): Promise<AppInfoResponse> => {
-  return new Promise((resolve, reject) => {
-    steamScraperStub.GetAppId(
-      request,
-      (err: any, response: Promise<AppInfoResponse>) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(response);
-        }
-      },
-    );
-  });
-};
-
-const request1: GameNameRequest = { gameName: "Hades" };
-const request2: GameNameRequest = { gameName: "Cyberpunk 2077" };
-const request3: AppIdRequest = { appId: "205100" };
-const request4: NReviewsRequest = { appId: "205100", n: 10 };
-const fac = new GrpcPromiseFactory(steamScraperStub);
-const getReviews = fac.createPromisifiedMethod("GetReviews");
-const getNReviews = fac.createPromisifiedMethod("GetNReviews");
-
-// Use the promisified function for multiple asynchronous calls
-
-promisifiedGetAppId(request1)
-  .then((response) => console.log("Response 1:", response))
-  .catch((error) => console.error("Error 1:", error));
-
-promisifiedGetAppId(request2)
-  .then((response) => console.log("Response 2:", response))
-  .catch((error) => console.error("Error 2:", error));
-
-getReviews(request3).then((response) => console.log("GetReviews:", response));
-getNReviews(request4).then((response) => console.log("GetNReviews:", response));
 
 app.get("/", (_, res: Response) => {
   res.send("Express + TypeScript Server");
@@ -69,3 +12,8 @@ app.get("/", (_, res: Response) => {
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
+
+const request1: GameNameRequest = { gameName: "Dishonored" };
+const request2: AppIdRequest = { appId: "205100" };
+
+insertGameWithReviews(request1, request2);
