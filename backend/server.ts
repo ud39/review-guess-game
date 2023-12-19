73f99bd105a8e09a30da6cd1d703ae1fe2cb4027
ud39 <ud-gaming@gmx.de>
2023-12-20 00:04:39 +0100

    Add: Cors to allowed request from the frontend

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,15 @@
 import express, { Express, Response } from "express";
+import cors from "cors";
 import { selectReviews } from "./databaseactions";
 
 const app: Express = express();
 const PORT = process.env["PORT"] || 3000;
+
+const allowedOrigins = ["http://localhost:4200"];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+app.use(cors(options));
 
 app.get("/", (_, res: Response) => {
   res.send("Express + TypeScript Server");
