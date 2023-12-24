02fc3103ebf4a09e97d32f27ca8f2803c4d4718b
ud39 <ud-gaming@gmx.de>
2023-12-24 22:40:21 +0100

    Add: Endpoints to get reviews for a game and initial random reviews.

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,6 @@
-import express, { Express, Response } from "express";
+import express, { Express, Response, Request } from "express";
 import cors from "cors";
-import { selectReviews } from "./databaseactions";
+import { selectInitialReviews, selectReviews } from "./databaseactions";
 
 const app: Express = express();
 const PORT = process.env["PORT"] || 3000;
@@ -9,14 +9,38 @@ const allowedOrigins = ["http://localhost:4200"];
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
 };
-app.use(cors(options));
 
+app.use(cors(options));
 app.get("/", (_, res: Response) => {
   res.send("Express + TypeScript Server");
+});
+
+type selectParams = {
+  gameTitle: string;
+  numberOfReviews: string;
+};
+
+app.get(
+  "/reviews",
+  async (req: Request<{}, {}, {}, selectParams>, res: Response) => {
+    console.log(req.query);
+    const { gameTitle = "DefaultGameTitle", numberOfReviews = "0" } = req.query;
+
+    const result = await selectReviews(
+      gameTitle,
+      parseInt(numberOfReviews, 10),
+    );
+    res.send(result);
+  },
+);
+
+app.get("/initialreviews", async (req: Request, res: Response) => {
+  console.log(req, res);
+  const gameWithReviews = await selectInitialReviews();
+  console.log(gameWithReviews);
+  return gameWithReviews;
 });
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
-
-selectReviews("Dishonored", 2);
