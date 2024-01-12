521a93ba24a354d05767d76dddc04c42b9f8f663
ud39 <ud-gaming@gmx.de>
2024-01-12 22:58:44 +0100

    Add: Skeletion logic to handle keyboard interaction.

--- frontend/src/app/guess-perspective/guess-perspective.component.ts
+++ frontend/src/app/guess-perspective/guess-perspective.component.ts
@@ -1,19 +1,35 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  ViewChildren,
+  QueryList,
+  inject,
+  HostListener,
+} from '@angular/core';
 import { ReviewCardComponent } from '../review-card/review-card.component';
 import { Review } from '../types/MyTypes';
 import { ScoreComponent } from '../score/score.component';
 import { FetchReviewsService } from '../fetch-reviews/fetch-reviews.service';
 import { GuessInputComponent } from '../guess-input/guess-input.component';
+import { TimerComponent } from '../timer/timer.component';
+import { ReviewSelectionService } from '../review-selection/review-selection.service';
+import { take, combineLatest, map } from 'rxjs';
 
 @Component({
   selector: 'app-guess-perspective',
   standalone: true,
-  imports: [ReviewCardComponent, ScoreComponent, GuessInputComponent],
-  providers: [FetchReviewsService],
+  imports: [
+    ReviewCardComponent,
+    ScoreComponent,
+    GuessInputComponent,
+    TimerComponent,
+  ],
+  providers: [FetchReviewsService, ReviewSelectionService],
   templateUrl: './guess-perspective.component.html',
   styleUrl: './guess-perspective.component.scss',
 })
 export class GuessPerspectiveComponent {
+  private reviewSelectionService = inject(ReviewSelectionService);
   @Input() reviews: Review[] = [
     {
       steam_id: '1',
@@ -42,5 +58,22 @@ export class GuessPerspectiveComponent {
   ];
   isLoading: boolean = true;
 
+  @ViewChildren(ReviewCardComponent)
+  reviewCards!: QueryList<ReviewCardComponent>;
+
   constructor() {}
+
+  handleCardClick(clickedCard: ReviewCardComponent): void {}
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent): void {
+    if (event.key === 'ArrowLeft') this.left();
+    if (event.key === 'ArrowRight') this.right();
+    if (event.key === 'Enter') {
+    }
+  }
+
+  left(): void {}
+
+  right(): void {}
 }
